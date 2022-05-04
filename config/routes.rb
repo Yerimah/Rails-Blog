540fb281dc5fc0b0c1ec37fae98e470d6027344a
Yerimah <danielyerimah@yahoo.com>
2022-05-04 23:40:38 +0100

    create routes manually

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+    resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end

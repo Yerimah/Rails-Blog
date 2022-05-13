e96039a48fd437c28759f5d79fde0a4d92c73d3c
Yerimah <danielyerimah@yahoo.com>
2022-05-13 16:40:17 +0100

    create forms to add post, add comments and like post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(
+      text: params['comment']['text'],
+      author_id: params[:user_id],
+      post_id: params[:post_id]
+    )
+
+    if @comment.save
+      @comment.update_comments_counter
+      redirect_to user_posts_path(params[:user_id]), notice: 'You have successfully created a comment'
+    else
+      redirect_to user_posts_path(params[:user_id]), notice: 'Error! comment could not be created'
+    end
+  end
+end

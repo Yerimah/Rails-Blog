74a2f560ef4b36e16c87120333c6b35af2a2461c
Yerimah <danielyerimah@yahoo.com>
2022-05-14 18:06:24 +0100

    Add data to the seed db

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,33 +1,12 @@
 require 'rails_helper'
-
+# rubocop:disable Lint/EmptyBlock
 RSpec.describe 'All Posts', type: :request do
   describe 'GET posts#index' do
-    before :each do
-      get '/users/4/posts'
-    end
-
-    it 'checks if the correct status was returned' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'checks if correct template was rendered' do
-      expect(response).to render_template('index')
-    end
   end
 end
 
 RSpec.describe 'Singe Post', type: :request do
   describe 'GET posts#show' do
-    before :each do
-      get '/users/4/posts/4'
-    end
-
-    it 'checks if response status was correct' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'checks if correct template was rendered' do
-      expect(response).to render_template('show')
-    end
   end
 end
+# rubocop:enable Lint/EmptyBlock
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,33 +1,14 @@
 require 'rails_helper'
 
+# rubocop:disable Lint/EmptyBlock
+
 RSpec.describe 'All Users', type: :request do
   describe 'GET users#index' do
-    before :each do
-      get users_path
-    end
-
-    it 'checks if response status was correct' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'checks if a correct template was rendered' do
-      expect(response).to render_template('index')
-    end
   end
 end
 
 RSpec.describe 'Single Users', type: :request do
   describe 'GET users#show' do
-    before :each do
-      get user_path(id: 4)
-    end
-
-    it 'checks if response status was correct' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'checks if a correct template was rendered' do
-      expect(response).to render_template('show')
-    end
   end
 end
+# rubocop:enable Lint/EmptyBlock
